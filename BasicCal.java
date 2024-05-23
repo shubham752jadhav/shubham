e25fa39fc115847dc18b84afdce2288bdfc33430
shubham752jadhav <jadhavshubham752m@gmail.com>
2024-05-23 15:25:21 +0530

    Add files via upload

---
+++ BasicCal.java
@@ -0,0 +1,49 @@
+package Assignment;
+import java.util.Scanner;
+public class BasicCal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner scanner = new Scanner(System.in);
+
+	        System.out.println("Enter first number: ");
+	        double num1 = scanner.nextDouble();
+
+	        System.out.println("Enter second number: ");
+	        double num2 = scanner.nextDouble();
+
+	        System.out.println("Enter operator (+, -, *, /): ");
+	        char operator = scanner.next().charAt(0);
+
+	        double result = 0;
+
+	        switch (operator) {
+	            case '+':
+	                result = num1 + num2;
+	                break;
+	            case '-':
+	                result = num1 - num2;
+	                break;
+	            case '*':
+	                result = num1 * num2;
+	                break;
+	            case '/':
+	                if (num2 != 0) {
+	                    result = num1 / num2;
+	                } else {
+	                    System.out.println("Error! Division by zero.");
+	                    return;
+	                }
+	                break;
+	            default:
+	                System.out.println("Error! Invalid operator.");
+	                return;
+	        }
+
+	        System.out.println("Result: " + result);
+
+	        scanner.close();
+
+	}
+
+}
